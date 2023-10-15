0fddcd2ea2ae65d34ada1b768124d7e7c1ea803a
Kiara-frank <128553889+Kiara-frank@users.noreply.github.com>
2023-10-14 22:53:34 -0300

    Delete etc directory

--- etc/conexion.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost", "root", "", "futbol") or die ("No hay conexion");
-mysqli_query($conexion, "SET NAMES 'utf8'");
-mysqli_select_db($conexion, "futbol") or die ("No hay base");
